ed485665b1c9cc95dd5e50e8281111f819ccd9f2
ladiza <ladizala@gmail.com>
2019-11-22 18:44:07 +0100

    add computing after second symbol click

--- calc-script.js
+++ calc-script.js
@@ -5,6 +5,8 @@ const display = document.querySelector('#disp');
 const memory = document.querySelector('#memory');
 const symbols = document.querySelectorAll('#symbol');
 
+let mem = {firstNum: 0, secondNum: 0, operator: ''};
+
 nums.forEach(number => {
   number.addEventListener('click', () => {
     if (display.textContent.length < 22) {
@@ -12,23 +14,77 @@ nums.forEach(number => {
     } else {
       alert('Display is full! You can enter max 22 numbers.')
     }
-    console.log(memory.textContent.length);
   });
 });
 
 symbols.forEach(symbol => {
   symbol.addEventListener('click', () => {
+
     if (display.textContent.length == 0) {
       alert('Write a number first!');
+    } else if (memory.textContent.includes('+') || 
+               memory.textContent.includes('-') ||
+               memory.textContent.includes('*') || 
+               memory.textContent.includes('/')) {
+      mem.secondNum = display.textContent;
+      let result = compute(mem.firstNum, mem.secondNum, mem.operator);
+
+      memory.textContent = result + ' ' + symbol.textContent;
+      mem.firstNum = result;
+      mem.operator = symbol.textContent;
+      console.log(mem.operator);
+      display.textContent = '';
     } else {
+      mem.firstNum = display.textContent;
+      mem.operator = symbol.textContent;
+      console.log(mem.operator);
       memory.textContent = display.textContent + ' ' + symbol.textContent;
       display.textContent = '';
     };
-  });
+  }); 
 });
+
+function compute (prvnicislo, druhecislo, picovinamezi) {
+  
+  let firstNum = parseInt(prvnicislo);
+  let secondNum = parseInt(druhecislo);
+  let result = null;
+  switch (picovinamezi) {
+    case '+':
+      result = sum(firstNum, secondNum);
+      break;
+    case '-':
+      result = subtract(firstNum, secondNum);
+      break;
+    case '*':
+      result = multiply(firstNum, secondNum);
+      break;
+    case '/':
+      result = divide(firstNum, secondNum);
+      break;
+  }
+  return result;
+}
+
+function sum (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
 
 clearEverything.addEventListener('click', () => {
   display.textContent = '';
+  memory.textContent = '';
 });
 
 erase.addEventListener('click', () => {
