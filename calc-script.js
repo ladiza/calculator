6328d3511b9f4dfb23de7624991664eedb1aed0e
ladiza <ladizala@gmail.com>
2019-11-22 16:35:25 +0100

    add symbol input and display in memory

--- calc-script.js
+++ calc-script.js
@@ -2,6 +2,8 @@ const nums = document.querySelectorAll('#num');
 const clearEverything = document.querySelector('.clear-everything');
 const erase = document.querySelector('.clear');
 const display = document.querySelector('#disp');
+const memory = document.querySelector('#memory');
+const symbols = document.querySelectorAll('#symbol');
 
 nums.forEach(number => {
   number.addEventListener('click', () => {
@@ -10,10 +12,20 @@ nums.forEach(number => {
     } else {
       alert('Display is full! You can enter max 22 numbers.')
     }
+    console.log(memory.textContent.length);
   });
 });
 
-
+symbols.forEach(symbol => {
+  symbol.addEventListener('click', () => {
+    if (display.textContent.length == 0) {
+      alert('Write a number first!');
+    } else {
+      memory.textContent = display.textContent + ' ' + symbol.textContent;
+      display.textContent = '';
+    };
+  });
+});
 
 clearEverything.addEventListener('click', () => {
   display.textContent = '';
